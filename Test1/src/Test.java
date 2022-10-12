62e08694717f0765a4b3643f002b579d64f87f4a
Andreas Nyström <andy.i.flaskan@gmail.com>
2022-10-12 10:45:26 +0200

    Initial Commit

---
+++ Test1/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int one = 1;
+        int two = 2;
+        
+        System.out.println(one + two);
+    }
+}
